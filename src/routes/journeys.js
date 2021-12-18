f1042653ef3ed2dcd9649bdf0204a7dc30bcae7a
HieuBm <bmhieus07@gmail.com>
2021-12-19 00:11:55 +0700

    add create journey page

--- src/routes/journeys.js
+++ src/routes/journeys.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const journeyController = require('../app/controllers/JourneyController')
 
 router.get('/create', journeyController.create)
+router.get('/manage', journeyController.manage)
 router.post('/store', journeyController.store)
 router.get('/:slug',journeyController.showdetail)
 
